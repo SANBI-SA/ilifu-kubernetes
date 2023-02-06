04b7bf248761f1cc8582e92a05d358932fcfe129
Eugene de Beste <eugene@debeste.co.za>
2023-02-06 16:54:52 +0200

    fix race condition: source venv before creating it

--- terraform/k8s_ansible.sh
+++ terraform/k8s_ansible.sh
@@ -4,8 +4,8 @@ sudo sed -i 's/nova.clouds.archive.ubuntu.com/ubuntu.mirror.ac.za/g' /etc/apt/so
 sudo apt update && sudo apt install -y git python3-venv
 chmod 600 /home/ubuntu/ilifu.pem
 source ~/ilifu-kubernetes-ansible/app-cred.sh
-source ~/ansible/bin/activate
 cd ~; python3 -m venv ansible
+source ~/ansible/bin/activate
 pip3 install -U pip
 pip3 install 'ansible>=2.9' 'openstacksdk<=0.98.999'
 cd ~/ilifu-kubernetes-ansible
