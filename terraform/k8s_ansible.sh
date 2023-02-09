299db285847f288852061a4e97c89f2f26c39ac7
Eugene de Beste <eugene@debeste.co.za>
2023-02-09 13:45:59 +0200

    Terraform remote provisioner and Ansible autorun (+ new Ansible dynamic inventory) (#2)
    * added dynamic openstack inventory and ansible changes
    
    * fix race condition: source venv before creating it

---
+++ terraform/k8s_ansible.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo sed -i 's/nova.clouds.archive.ubuntu.com/ubuntu.mirror.ac.za/g' /etc/apt/sources.list
+sudo apt update && sudo apt install -y git python3-venv
+chmod 600 /home/ubuntu/ilifu.pem
+source ~/ilifu-kubernetes-ansible/app-cred.sh
+cd ~; python3 -m venv ansible
+source ~/ansible/bin/activate
+pip3 install -U pip
+pip3 install 'ansible>=2.9' 'openstacksdk<=0.98.999'
+cd ~/ilifu-kubernetes-ansible
+ansible-galaxy install -r requirements.yml
+ansible-playbook -i openstack.yml playbook.yml
